0319e79482aef2f1944c3a671a807d36fc353baf
yxcho <choyangxuan@me.com>
2020-04-21 00:02:35 +0800

    cms - posts

--- admin/functions.php
+++ admin/functions.php
@@ -1,4 +1,14 @@
 <?php
+
+function confirmQuery($result){
+    global $connection;
+    if (!$result) {
+        die("QUERY FAILED " . mysqli_error($connection));
+    }
+}
+
+
+
 function insert_category()
 {
 
--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -41,15 +41,15 @@
                 <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="#">View all posts</a>
+                        <a href="./posts.php">View all posts</a>
                     </li>
                     <li>
-                        <a href="#">Add post</a>
+                        <a href="posts.php?source=add_post">Add post</a>
                     </li>
                 </ul>
             </li>
             <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                <a href="./categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
             </li>
 
             <li class="active">
